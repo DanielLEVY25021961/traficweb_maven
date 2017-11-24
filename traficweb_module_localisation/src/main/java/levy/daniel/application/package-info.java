2d62aa209c66ff3068feadf29b842fdec090bd14
DanielLevy25021961 <dany.levy@free.fr>
2017-11-24 16:27:12 +0100

    COMMIT_DL_DOMICILE

--- traficweb_module_localisation/src/main/java/levy/daniel/application/package-info.java
+++ traficweb_module_localisation/src/main/java/levy/daniel/application/package-info.java
@@ -1,7 +1,44 @@
 /**
  * class package-info :<br/>
- * .<br/>
+ * <ul>
+ * <li>
+ * Ce package <b>levy.daniel.application</b> correspond au 
+ * <b>GroupId</b> dans MAVEN.
+ * </li>
+ * <li>
+ * L'architecture applicative retenue est de type SOA.
+ * </li>
  * <br/>
+ * <li>
+ * <img src="../../../../../../javadoc/images/architecture_applicative.png" 
+ * alt="architecture applicative" border="1" align="center" />
+ * </li>
+ * <br/>
+ * <li>
+ * L'arborescence du projet est la suivante :</li>
+ * </li>
+ * <br/>
+ * <li>
+ * <img src="../../../../../../javadoc/images/arborescence.png" 
+ * alt="arborescence" border="1" align="center" />
+ * </li>
+ * <br/>
+ * <li>Le projet traite des <b>LOCALISATIONS</b> 
+ * conformes au Modèle d'Echange 
+ * des Référentiels Inter-Urbains <b>(MERIU v2)</b> du Ministère.</li>
+ * <br/>
+ * <li>
+ * Voici un exemple d'un même point localisé en 
+ * [PR + abscisse], [PLO de type PR + abscisse]
+ * , [PLO de construction + abscisse (négative)] et 
+ * localisation géométrique :
+ * </li>
+ * <br/>
+ * <li>
+ * <img src="../../../../../../javadoc/images/Route_A2_Dept59.png" 
+ * alt="image de 4 localisations d'un même point" border="1" align="center" />
+ * </li>
+ * </ul>
  *
  * - Exemple d'utilisation :<br/>
  *<br/>
---
+++ traficweb_module_localisation/src/test/java/levy/daniel/application/model/metier/localisation/localisationlineaire/impl/LocalisationPRTest.java
@@ -0,0 +1,78 @@
+package levy.daniel.application.model.metier.localisation.localisationlineaire.impl;
+
+import static org.junit.Assert.fail;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Test;
+
+
+/**
+ * class LocalisationPRTest :<br/>
+ * <ul>
+ * <li>Test JUnit de la classe LocalisationPR.</li>
+ * </ul>
+ * <br/>
+ *
+ * - Exemple d'utilisation :<br/>
+ *<br/>
+ * 
+ * - Mots-clé :<br/>
+ * <br/>
+ *
+ * - Dépendances :<br/>
+ * <br/>
+ *
+ *
+ * @author dan Lévy
+ * @version 1.0
+ * @since 24 nov. 2017
+ *
+ */
+public class LocalisationPRTest {
+
+	// ************************ATTRIBUTS************************************/
+	
+	/**
+	 * AFFICHAGE_GENERAL : Boolean :<br/>
+	 * Boolean qui commande l'affichage pour tous les tests.<br/>
+	 */
+	public static final Boolean AFFICHAGE_GENERAL = true;
+
+	
+	/**
+	 * TIRETS : String :<br/>
+	 * "--------------------------------------------------------".<br/>
+	 */
+	public static final String TIRETS 
+	= "--------------------------------------------------------";
+
+
+	/**
+	 * LOG : Log : 
+	 * Logger pour Log4j (utilisant commons-logging).
+	 */
+	private static final Log LOG = LogFactory.getLog(LocalisationPRTest.class);
+
+
+	// *************************METHODES************************************/
+	
+	 /**
+	 * method CONSTRUCTEUR LocalisationPRTest() :<br/>
+	 * CONSTRUCTEUR D'ARITE NULLE.<br/>
+	 * <br/>
+	 */
+	public LocalisationPRTest() {
+		super();
+	} // Fin de CONSTRUCTEUR D'ARITE NULLE.________________________________
+	
+
+
+	@Test
+	public void test() {
+
+		fail("Not yet implemented");
+	}
+
+
+}
